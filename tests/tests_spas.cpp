0b4a0a8c0b442ba58817824f5f48f689e00cab64
Rui Figueira <ruimvfigueira@gmail.com>
2017-01-29 18:49:59 +0000

    Some tests

--- tests/tests_spas.cpp
+++ tests/tests_spas.cpp
@@ -11,11 +11,32 @@ using namespace spas;
 // On windows we use epmap (port 135)
 #define SERVER_UNUSABLE_PORT 135
 
-
 using namespace cz::spas;
+
+#define CHECK_CZSPAS_EQUAL(expected, ec) \
+	if ((ec.code)!=(Error::Code::expected)) { \
+		UnitTest::CheckEqual(*UnitTest::CurrentTest::Results(), Error(Error::Code::expected).msg(), ec.msg(),UnitTest::TestDetails(*UnitTest::CurrentTest::Details(), __LINE__)); \
+	}
+#define CHECK_CZSPAS(ec) CHECK_CZSPAS_EQUAL(Success, ec)
 
 SUITE(CZSPAS)
 {
+
+TEST(Acceptor_listen_ok)
+{
+	Service io;
+	Acceptor ac(io);
+	auto ec = ac.listen(SERVER_PORT, 1);
+	CHECK_CZSPAS(ec);
+}
+
+TEST(Acceptor_listen_failure)
+{
+	Service io;
+	Acceptor ac(io);
+	auto ec = ac.listen(SERVER_UNUSABLE_PORT, 1);
+	CHECK_CZSPAS_EQUAL(Other, ec);
+}
 
 TEST(Service1)
 {
